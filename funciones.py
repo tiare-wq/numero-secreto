56753b63b83783c396a94fc77cbe09f0b7509ec3
Titi <tiaresalazar.g@gmail.com>
2025-07-20 16:10:29 -0400

    Actualización del juego con mejoras en la gestión de datos

--- funciones.py
+++ funciones.py
@@ -1,15 +1,129 @@
+import json
+import os
 import time
 
 # Actualiza y muestra los resultados del usuario
-def actualizar_resultados(nombre, jugadores):
-    jugadores[nombre]["Partidas ganadas"] = jugadores[nombre]["Partidas ganadas"] + 1
+def actualizar_resultados(jugador, resultado):
+    if resultado:
+        # Actualiza las partidas ganadas
+        jugador["Partidas ganadas"] += 1
 
-    print(f"📍 Estadísticas de {nombre}:\n")
-    for p, v in jugadores[nombre].items():
+    jugador["Partidas jugadas"] += 1
+
+    # Muestra los resultados
+    print(f"📍 Estadísticas de {jugador["Nombre"]}:\n")
+    for p, v in jugador.items():
         print("▪ ", p, ': ', v, sep='')
+    print()
+
+# Actualiza el TOP si el ganador está dentro de los 5 mejores puntajes
+def actualizar_top(jugador, top):
+    entro_al_top = False
+    p_max = next((n["Puesto"] for n in top if n["Nombre"] == jugador["Nombre"]), 6) # Establece índice mínimo de 'cambiar_lugar_ranking'
+
+    # Comparar puesto por puesto de mayor a menor cantidad de victorias.
+    for i in sorted(top, key=lambda x: x["Puesto"], reverse=True):
+        # Si es mayor guardar el puesto del ranking del jugador que reemplaza
+        if jugador["Partidas ganadas"] > i["Partidas ganadas"]:
+            jugador["Puesto"] = i["Puesto"]
+            entro_al_top = cambiar_lugar_ranking(i["Puesto"], p_max, top)
+            break
+        # Si tiene la misma cantidad de ganadas, ubicar primero al que tenga menos partidas jugadas
+        elif jugador["Partidas ganadas"] == i["Partidas ganadas"]:
+            if jugador["Partidas jugadas"] < i["Partidas jugadas"]:
+                jugador["Puesto"] = i["Puesto"]
+                entro_al_top = cambiar_lugar_ranking(i["Puesto"], p_max, top)
+                break       
+            # Si tienen la misma cantidad en ambas partidas, ubicar primero a la victoria más reciente
+            elif jugador["Partidas jugadas"] == i["Partidas jugadas"]:
+                jugador["Puesto"] = i["Puesto"]
+                entro_al_top = cambiar_lugar_ranking(i["Puesto"], p_max, top)
+                break
+        
+    # Agregar al nuevo jugador
+    if entro_al_top:
+        top.append(jugador)
+        if p_max == 6:
+            print("Jugador entró en el TOP")
+
+    # Eliminar al último jugador
+    borrado = next((t for t in top if t["Puesto"] == 6), None)
+    if borrado:
+        top.remove(borrado)
+
+# Actualiza el ranking de los demás jugadores
+def cambiar_lugar_ranking(p_min, p_max, top):
+    for j in sorted(top, key=lambda x: x["Puesto"], reverse=True):
+        if p_min <= j["Puesto"] < p_max:
+            j["Puesto"] += 1
+
+    return True
+
+# Carga listas de 'jugadores' y 'top', sin lanzar error si no existe
+def cargar_archivos(ruta):
+    if os.path.exists(ruta):
+        try:
+            with open(ruta, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            print(f"Advertencia: El archivo {ruta} está dañado. Se usará una lista vacía.")
+            return []
+    else:
+        return []
+
+# Elimina los datos de las listas de 'jugadores' y 'top'
+def eliminar_datos(jugadores, top):
+    del jugadores[:]
+    del top[:]
+    print("Estadísticas reiniciadas correctamente.\n")
+
+# Garantiza que la listas 'jugadores' y 'top' siempre se guarden en la subcarpeta correcta
+def garantizar_ruta():
+    CARPETA_BASE = os.path.dirname(__file__)  # Carpeta donde está el script actual
+    RUTA_DATOS = os.path.join(CARPETA_BASE, "datos")
+    os.makedirs(RUTA_DATOS, exist_ok=True) # Crea '/datos' si no existe, sin lanzar error si ya existe
+    return RUTA_DATOS
+
+
+# Guarda listas 'jugadores' y 'top' en minijuego/datos/nombre_del_archivo
+def guardar_archivos(jugadores, top, ruta_j, ruta_t):
+    top.sort(key=lambda x: x["Puesto"]) # Ordena la lista de 'top' por su puesto en el ranking
+    with open(ruta_j, 'w', encoding='utf-8') as f:
+        json.dump(jugadores, f, indent=4, ensure_ascii=False) # 'ensure_ascii=False' para caracteres especiales
+    with open(ruta_t, 'w', encoding='utf-8') as f:
+        json.dump(top, f, indent=4, ensure_ascii=False)
+
+# Agrega un nuevo jugador o actualiza su ficha
+def ingresar_jugador(nombre, jugadores):
+    # Verifica si el nombre del jugador está ingresado, de lo contrario guarda 'None'
+    jugador = next((j for j in jugadores if j["Nombre"] == nombre), None)
+    if jugador:
+        # Mostrar que el nombre ya está registrado
+        respuesta = f"El jugador '{nombre}' ya ha sido registrado. ¿Deseas continuar de todos modos? (s/n): "
+        reusar_nombre = validar_continuar(respuesta)
+
+        # Solicitar nuevo nombre
+        if reusar_nombre == 'n':
+            nombre = validar_nombre()
+            return ingresar_jugador(nombre, jugadores)
+        
+        return jugador
+
+    jugador = {"Nombre": nombre, "Partidas jugadas": 0, "Partidas ganadas": 0}
+    jugadores.append(jugador)
+    print("Nuevo jugador registrado.\n")
+    return jugador
+
+# Menú
+def menu():
+    print("\n⚔ MENÚ DE JUEGO")
+    print("1. Nueva partida")
+    print("2. Ver TOP 5")
+    print("3. Reiniciar archivos")
+    print("4. Salir\n")
 
 # Itera un máximo de tres veces la pregunta de cuál es el número secreto
-def minijuego(num1, num2, num_secreto):
+def minijuego(num_secreto):
     intentos = []
     n = 3
     for i in range(3):
@@ -31,33 +145,41 @@ def minijuego(num1, num2, num_secreto):
 
         if i < 2:
             pistas(i, num_secreto, intentos) # Pistas para hacer la experiencia del jugador más divertida
-        
-    print(f"\nEl número era: {num_secreto}. ¡Pero estuviste cerca! Vuelve a intentarlo cuando quieras ^^.\n")
+    
+    time.sleep(1)    
+    print(f"El número secreto era: {num_secreto}. ¡Pero estuviste cerca! Vuelve a intentarlo cuando quieras ^^.\n")
+
+    time.sleep(1)
 
 # Mostrar todos los resultados cuanto se cierra el programa
 def mostrar_todos_los_resultados(jugadores):
     for j in jugadores:
-        print("👤", j)
-        for p, v in jugadores[j].items():
-            print("▪ ", p, ': ', v, sep='')
+        for p, v in j.items():
+            if p == "Nombre":
+                print("👤", v)
+            elif p == "Puesto":
+                print(f"Jugador se ubica en puesto {v} del ranking.")
+            else:
+                print("▪ ", p, ': ', v, sep='')
         print()
 
-# Agrega un nuevo jugador o actualiza su ficha
-def nueva_partida(nombre, jugadores):
-    if nombre not in jugadores:
-        jugadores[nombre] = {"Partidas jugadas": 1, "Partidas ganadas": 0}
-    else:
-        jugadores[nombre]["Partidas jugadas"] = jugadores[nombre]["Partidas jugadas"] + 1
+# Mostrar top
+def mostrar_top(top):
+    for t in sorted(top, key=lambda x: x["Puesto"]):
+        print(f"{t["Puesto"]}: {t["Nombre"]}")
+        print("▪ Partidas ganadas:", t["Partidas ganadas"])
+        print("▪ Partidas jugadas:", t["Partidas jugadas"])
+        print()
 
 # Entrega pistas según la cantidad de intentos
 def pistas(i, num_secreto, intentos):
     # Cuenta los intentos restantes
     print("¡Ese no era el número! ¡Recórcholis!")
     if i == 0:
-        print("Dos intentos restantes")
+        print("Intentos restantes: 2")
 
     else:
-        print("Un intento restante")
+        print("Intento restante: 1")
 
     comparacion = "mayor" if intentos[i] > num_secreto else "menor"
     print(f"Pista: El número ingresado es {comparacion} que el número secreto.\n")
@@ -73,9 +195,9 @@ def pistas(i, num_secreto, intentos):
             print(f"Pista extra: El número secreto está a la misma distancia de {intentos[0]} que de {intentos[1]}...\n")
 
 # Verifica si el jugador quiere o no seguir jugando
-def validar_continuar():
+def validar_continuar(continuar):
     while True:
-        continuar = input("\n¿Deseas seguir jugando? (s/n): ").strip().lower()
+        continuar = input(continuar).strip().lower()
         if continuar in ['s', 'n']:
             return continuar
         print("Debes ingresar s (sí) o n (no).\n")
@@ -83,7 +205,7 @@ def validar_continuar():
 # Verifica que es un nombre válido
 def validar_nombre():
     while True:
-        nombre = input("Ingresa tu nombre: ").strip().title()
+        nombre = input("Ingresa tu nombre: ").strip()
         print()
         if any(l.isalpha() for l in nombre):
             return nombre
@@ -97,7 +219,7 @@ def verificar_num(i, intento, num_secreto):
     if intento == num_secreto:
         print(correcto)
         print("No puede ser... Has adivinado el número secreto.")
-        print("¡Felicitaciones, pudiste adivinar! ¡El tesoro es tuyo!\n")
+        print("¡Felicitaciones, pudiste adivinar! ¡El tesoro es tuyo! 👑💰\n")
         return True
     elif i == 0:
         print(incorrecto)
--- main.py
+++ main.py
@@ -1,57 +1,123 @@
 """ 'Número secreto' es un juego que consiste en ingresar dos números, un menor y uno mayor, que serán el rango del número secreto,
 un número random. Se considera que es una parte del rompecabezas de un juego de aventura más grande. El Nigromante no mata al jugador,
-solo no le da el tesoro.
+solo no le da el tesoro. """
 
-Aún no domino bien JSON y txt por lo tanto no decidí no implementarlo. """
-
-from funciones_NS import actualizar_resultados, minijuego, mostrar_todos_los_resultados, nueva_partida, validar_continuar, validar_nombre
+import funciones
+import os
 import random
 import time
 
 # Función principal
 def main():
-    jugadores = {}
+    ruta = funciones.garantizar_ruta() # Crea carpeta 'datos' si no existe, sin lanzar error si ya existe
+
+    ruta_jugadores = os.path.join(ruta, "jugadores.json")
+    jugadores = funciones.cargar_archivos(ruta_jugadores)
+
+    ruta_top = os.path.join(ruta, "top.json")
+    top = funciones.cargar_archivos(ruta_top)
+    
 
     while True:
-        print("🔮 NUEVA PARTIDA\n")
-        nombre = validar_nombre()
-        nueva_partida(nombre, jugadores)
+        
+        funciones.menu()
 
-        # Solicita dos números, con un rango mínimo de 15. Se utiliza try/except para garantizar que sean números enteros
         while True:
             try:
-                num1 = int(input("Ingrese el primer número (piénsalo bien...): "))
-                break
-            except ValueError:
-                print("¿Y eso que es? ¿Se come? No, solo se aceptan números.\n")
-        while True:
-            try:
-                num2 = int(input("Ingrese el segundo número (debe ser MAYOR que el primero): "))
-
-                if num2 - num1 >= 15: # Comprueba el rango
+                opcion = int(input("Elija una opción: "))
+                if opcion in range(1, 5):
                     break
-                print("¡Al menos 15 números más grande! O sino el mago no aceptará...\n")
+                print("Opción fuera de rango.\n")
             except ValueError:
-                print("¿Y eso que es? ¿Se come? No, solo se aceptan números.\n")
+                print("Debes ingresar valores numéricos.\n")
 
-        num_secreto = random.randint(num1, num2) # Genera el número secreto
+        print()
+        if opcion == 1:
+            print("🔮 NUEVA PARTIDA\n")
+            nombre = funciones.validar_nombre()
+            jugador = funciones.ingresar_jugador(nombre, jugadores)
 
-        print("Oh! ¡Entraste a mi juego! ¡Para obtener mi tesoro debes adivinar el número secreto! ¡Pero nadie me lo quitará! ¡JAJAJA!\n")
-        time.sleep(2)
-        print("📢 ¡Recuerda que el número secreto está entre el rango que diste!\n")
+            # Solicita dos números, con un rango mínimo de 15. Se utiliza try/except para garantizar que sean números enteros
+            while True:
+                try:
+                    num1 = int(input("Ingrese el primer número (piénsalo bien...): "))
+                    break
+                except ValueError:
+                    print("¿Y eso que es? ¿Se come? No, solo se aceptan números.\n")
+            while True:
+                try:
+                    num2 = int(input("Ingrese el segundo número (debe ser MAYOR que el primero): "))
 
-        resultado = minijuego(num1, num2, num_secreto)
+                    if num2 - num1 >= 15: # Comprueba el rango
+                        break
+                    print("¡Al menos 15 números más grande! El mago ya está poniendo caritas...\n")
+                except ValueError:
+                    print("¿Y eso que es? ¿Se come? No, solo se aceptan números.\n")
 
-        if resultado:
+            num_secreto = random.randint(num1, num2) # Genera el número secreto
+
+            print("Oh! ¡Entraste a mi juego! ¡Para obtener mi tesoro debes adivinar el número secreto! ¡Pero nadie me lo quitará! ¡JAJAJA!\n")
+            time.sleep(1.5)
+            print("📢 ¡Recuerda que el número secreto está entre el rango que diste!\n")
+
+            resultado = funciones.minijuego(num_secreto)
+
+            if resultado:
+                time.sleep(1)
+                print(f"Felicitaciones {jugador["Nombre"]}, has ganado el reto.\n")
+
+            funciones.actualizar_resultados(jugador, resultado)
+            # Valida si tiene las misma cantidad o más partidas ganadas que los jugadores del top
+            validar_ranking = any(t["Partidas ganadas"] <= jugador["Partidas ganadas"] for t in top)
+            if top and validar_ranking:
+                funciones.actualizar_top(jugador, top)
+            elif not top:
+                jugador["Puesto"] = 1
+                top.append(jugador)
+                print("Jugador entró en el TOP\n")
+            
+            continuar = "¿Deseas volver al menú principal? (s/n): "
+            respuesta = funciones.validar_continuar(continuar)
+            
+            if respuesta == 'n':
+                print()
+                funciones.guardar_archivos(jugadores, top, ruta_jugadores, ruta_top) # Guardar archivos
+
+                time.sleep(1)
+                funciones.mostrar_todos_los_resultados(jugadores)
+                break
+            
             time.sleep(1)
-            print(f"Felicitaciones {nombre}, has ganado el reto.\n")
+            continue
+        
+        elif opcion == 2:
+            if top:
+                print("✨ TOP 5\n")
+                funciones.mostrar_top(top)
+            else:
+                print("Aún no hay ranking de jugadores\n")
 
-            actualizar_resultados(nombre, jugadores)
+        elif opcion == 3:
+            borrar_archivos = "¿Estás seguro(a) que deseas borrar los datos de todos los jugadores? (s/n): "
+            respuesta = funciones.validar_continuar(borrar_archivos)
+
+            # Eliminar archivos
+            if respuesta == 's':
+                funciones.eliminar_datos(jugadores, top)
+            else:
+                print()
         
-        respuesta = validar_continuar()
-        
-        if respuesta == 'n':
-            mostrar_todos_los_resultados(jugadores)
+        else:
+            funciones.guardar_archivos(jugadores, top, ruta_jugadores, ruta_top) # Guardar archivos
+
+            funciones.mostrar_todos_los_resultados(jugadores)
+            time.sleep(1)
+
+            print("Saliendo del programa...")
+            time.sleep(1)
             break
+
+        time.sleep(1)
+        input("Presiona ENTER para continuar")
 
 main()
